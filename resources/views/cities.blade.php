f2a3ea81da43fcde4e7464258f2ec23752837ffd
Emmanu <kbisansaba@gmail.com>
2016-07-13 10:38:28 +0300

    Collapsable Table

--- resources/views/cities.blade.php
+++ resources/views/cities.blade.php
@@ -5,65 +5,232 @@
             <table class="table table-striped table-condensed table-responsive" style="margin-top: -28px; margin-left: -12px; margin-right: -10px;">
                 <thead style="background-color: #435970; color: white; font-size: 12px; font-weight: bold">
                 <tr>
+                    <th></th>
                     <th>Route</th>
-                    <th>From Area</th>
-                    <th>Passing Through</th>
-                    <th>To Area</th>
-                    <th>Stations</th>
-                </tr>
+                    <th>Descriptions</th>
                 </thead>
                 <tbody>
+                <tr data-toggle="collapse" data-target="#demo1" class="accordion-toggle">
+                    <td><button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-eye-open"></span></button></td>
+                    <td>Tabora- Kaliua- Mpanda</td>
+                    <td></td>
+                </tr>
                 <tr>
-                    <td width="150">Tabora to Mpanda</td>
-                    <td>Tabora</td>
-                    <td>Kaliua</td>
-                    <td>Mpanda</td>
-                    <td>MABAMA</td>
-                </tr>
+                    <td colspan="12" class="hiddenRow"><div class="accordian-body collapse" id="demo1">
+                            <table class="table table-striped">
+                                <thead>
                                 <tr>
-                    <td width="150">Tabora to Mpanda</td>
-                    <td>Tabora</td>
-                    <td>Kaliua</td>
-                    <td>Mpanda</td>
-                    <td>USOKE</td>
-                </tr>
+                                    <th>Access Key</th>
+                                    <th>Secret Key</th>
+                                    <th>Status </th>
+                                    <th> Created</th>
+                                    <th> Expires</th>
+                                    <th>Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
                                 <tr>
-                    <td width="150">Tabora to Mpanda</td>
-                    <td>Tabora</td>
-                    <td>Kaliua</td>
-                    <td>Mpanda</td>
-                    <td>URAMBO</td>
+                                    <td>access-key-1</td>
+                                    <td>secretKey-1</td>
+                                    <td>Status</td>
+                                    <td>some date</td>
+                                    <td>some date</td>
+                                    <td><a href="#" class="btn btn-default btn-sm"><i class="glyphicon glyphicon-cog"></i></a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+
+                        </div> </td>
                 </tr>
+                <tr data-toggle="collapse" data-target="#demo2" class="accordion-toggle">
+                    <td><button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-eye-open"></span></button></td>
+                    <td>Dar es Salaam to Kigoma</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td colspan="12" class="hiddenRow"><div class="accordian-body collapse" id="demo2">
+                            <table class="table table-striped">
+                                <thead>
                                 <tr>
-                    <td width="150">Tabora to Mpanda</td>
-                    <td>Tabora</td>
-                    <td>Kaliua</td>
-                    <td>Mpanda</td>
-                    <td>KALIUA</td>
-                </tr>
+                                    <th>Access Key</th>
+                                    <th>Secret Key</th>
+                                    <th>Status </th>
+                                    <th> Created</th>
+                                    <th> Expires</th>
+                                    <th>Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
                                 <tr>
-                    <td width="150">Tabora to Mpanda</td>
-                    <td>Tabora</td>
-                    <td>Kaliua</td>
-                    <td>Mpanda</td>
-                    <td>UYUMBU</td>
+                                    <td>access-key-1</td>
+                                    <td>secretKey-1</td>
+                                    <td>Status</td>
+                                    <td>some date</td>
+                                    <td>some date</td>
+                                    <td><a href="#" class="btn btn-default btn-sm"><i class="glyphicon glyphicon-cog"></i></a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+
+                        </div> </td>
                 </tr>
+                <tr data-toggle="collapse" data-target="#demo3" class="accordion-toggle">
+                    <td><button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-eye-open"></span></button></td>
+                    <td>Tabora - kaliua to Kigoma</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td colspan="12" class="hiddenRow"><div class="accordian-body collapse" id="demo3">
+                            <table class="table table-striped">
+                                <thead>
                                 <tr>
-                    <td width="150">Tabora to Mpanda</td>
-                    <td>Tabora</td>
-                    <td>Kaliua</td>
-                    <td>Mpanda</td>
-                    <td>KENGEME</td>
-                </tr>
+                                    <th>Access Key</th>
+                                    <th>Secret Key</th>
+                                    <th>Status </th>
+                                    <th> Created</th>
+                                    <th> Expires</th>
+                                    <th>Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
                                 <tr>
-                    <td width="150">Tabora to Mpanda</td>
-                    <td>Tabora</td>
-                    <td>Kaliua</td>
-                    <td>Mpanda</td>
-                    <td>LUMBE</td>
+                                    <td>access-key-1</td>
+                                    <td>secretKey-1</td>
+                                    <td>Status</td>
+                                    <td>some date</td>
+                                    <td>some date</td>
+                                    <td><a href="#" class="btn btn-default btn-sm"><i class="glyphicon glyphicon-cog"></i></a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+
+                        </div> </td>
                 </tr>
+                <tr data-toggle="collapse" data-target="#demo4" class="accordion-toggle">
+                    <td><button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-eye-open"></span></button></td>
+                    <td>Ubungo To Posta</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td colspan="12" class="hiddenRow"><div class="accordian-body collapse" id="demo4">
+                            <table class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th>Access Key</th>
+                                    <th>Secret Key</th>
+                                    <th>Status </th>
+                                    <th> Created</th>
+                                    <th> Expires</th>
+                                    <th>Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>access-key-1</td>
+                                    <td>secretKey-1</td>
+                                    <td>Status</td>
+                                    <td>some date</td>
+                                    <td>some date</td>
+                                    <td><a href="#" class="btn btn-default btn-sm"><i class="glyphicon glyphicon-cog"></i></a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+
+                        </div> </td>
+                </tr>
+                <tr data-toggle="collapse" data-target="#demo5" class="accordion-toggle">
+                    <td><button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-eye-open"></span></button></td>
+                    <td>Dar es Salaam to Tabora</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td colspan="12" class="hiddenRow"><div class="accordian-body collapse" id="demo5">
+                            <table class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th>Access Key</th>
+                                    <th>Secret Key</th>
+                                    <th>Status </th>
+                                    <th> Created</th>
+                                    <th> Expires</th>
+                                    <th>Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>access-key-1</td>
+                                    <td>secretKey-1</td>
+                                    <td>Status</td>
+                                    <td>some date</td>
+                                    <td>some date</td>
+                                    <td><a href="#" class="btn btn-default btn-sm"><i class="glyphicon glyphicon-cog"></i></a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+
+                        </div> </td>
+                </tr>
+                {{--<tr>--}}
+                    {{--<td width="150">Tabora to Mpanda</td>--}}
+                    {{--<td>Tabora</td>--}}
+                    {{--<td>Kaliua</td>--}}
+                    {{--<td>Mpanda</td>--}}
+                    {{--<td>MABAMA</td>--}}
+                {{--</tr>--}}
+                                {{--<tr>--}}
+                    {{--<td width="150">Tabora to Mpanda</td>--}}
+                    {{--<td>Tabora</td>--}}
+                    {{--<td>Kaliua</td>--}}
+                    {{--<td>Mpanda</td>--}}
+                    {{--<td>USOKE</td>--}}
+                {{--</tr>--}}
+                                {{--<tr>--}}
+                    {{--<td width="150">Tabora to Mpanda</td>--}}
+                    {{--<td>Tabora</td>--}}
+                    {{--<td>Kaliua</td>--}}
+                    {{--<td>Mpanda</td>--}}
+                    {{--<td>URAMBO</td>--}}
+                {{--</tr>--}}
+                                {{--<tr>--}}
+                    {{--<td width="150">Tabora to Mpanda</td>--}}
+                    {{--<td>Tabora</td>--}}
+                    {{--<td>Kaliua</td>--}}
+                    {{--<td>Mpanda</td>--}}
+                    {{--<td>KALIUA</td>--}}
+                {{--</tr>--}}
+                                {{--<tr>--}}
+                    {{--<td width="150">Tabora to Mpanda</td>--}}
+                    {{--<td>Tabora</td>--}}
+                    {{--<td>Kaliua</td>--}}
+                    {{--<td>Mpanda</td>--}}
+                    {{--<td>UYUMBU</td>--}}
+                {{--</tr>--}}
+                                {{--<tr>--}}
+                    {{--<td width="150">Tabora to Mpanda</td>--}}
+                    {{--<td>Tabora</td>--}}
+                    {{--<td>Kaliua</td>--}}
+                    {{--<td>Mpanda</td>--}}
+                    {{--<td>KENGEME</td>--}}
+                {{--</tr>--}}
+                                {{--<tr>--}}
+                    {{--<td width="150">Tabora to Mpanda</td>--}}
+                    {{--<td>Tabora</td>--}}
+                    {{--<td>Kaliua</td>--}}
+                    {{--<td>Mpanda</td>--}}
+                    {{--<td>LUMBE</td>--}}
+                {{--</tr>--}}
                 </tbody>
             </table>
         </div>
     </div>
+
+
+
+
+
 @endsection
